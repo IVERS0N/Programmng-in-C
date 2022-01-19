1f3cb534d4aa0fcf2c9dacb55a1f2540d72769e3
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-01-19 16:04:13 +0100

    Update main.c

--- exercici2/main.c
+++ exercici2/main.c
@@ -1,1 +1,25 @@
 
+#include<stdio.h>
+
+void main(){
+	int inputSecond;
+
+	int hours,minutes,seconds;
+	int remainingSeconds;
+
+	int secondsInHour = 60 * 60;
+	int secondsInMinute = 60;
+
+	printf("Enter seconds : ");
+	scanf("%d",&inputSecond);
+
+	hours = (inputSecond/secondsInHour);
+
+	remainingSeconds = inputSecond - (hours * secondsInHour);
+	minutes = remainingSeconds/secondsInMinute;
+
+	remainingSeconds = remainingSeconds - (minutes*secondsInMinute);
+	seconds = remainingSeconds;
+
+	printf("%d hour, %d minutes and %d seconds",hours,minutes,seconds);
+}
