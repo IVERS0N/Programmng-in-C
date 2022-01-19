cc86edadd52d8b63147a4aac887c3bfb2ec83d34
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-01-19 16:54:57 +0100

    Update main.c

--- exercici4/main.c
+++ exercici4/main.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+
+void main()
+{
+    int grade, pass, fail, total_test;
+    
+    pass = 0;
+    fail = 0;
+
+    do {
+        printf("\nEnter grade:\n");
+        scanf("%d", &grade);
+
+        if (grade < 10 && grade >= 5)
+            pass = pass + 1;
+        else 
+            fail = fail + 1;
+        printf("\nPass: %d", pass);
+        printf("\nFail: %d", fail);
+        total_test = (pass + fail);
+    }
+    
+    while (total_test <= 14);
+    
+}
