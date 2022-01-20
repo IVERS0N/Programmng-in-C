bd3d7babbb32ba1fd4246ead54d3e0e78d1cfa64
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-01-20 16:57:17 +0100

    Update main.c

--- exercici7/main.c
+++ exercici7/main.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h> 
+void main() 
+{ 
+    int size=0; 
+    int arr[size]; 
+    printf("input size of array: "); 
+    scanf("%d",&size); 
+    printf("\ninput elements of array:  "); 
+    for(int i=0;i<size;i++){ 
+        scanf("%d",&arr[i]); 
+    } 
+    printf("\nThe array  is "); 
+    for(int i=0;i<size;i++){ 
+        printf("%d",arr[i]); 
+    }
+} 
