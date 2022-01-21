e6a925d5dde4dbd48a71660a0628332d19719774
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-01-21 16:53:26 +0100

    Update main.c

--- exercici7/main.c
+++ exercici7/main.c
@@ -1,17 +1,42 @@
 
-#include <stdio.h> 
-void main() 
-{ 
-    int size=0; 
-    int arr[size]; 
-    printf("input size of array: "); 
-    scanf("%d",&size); 
-    printf("\ninput elements of array:  "); 
-    for(int i=0;i<size;i++){ 
-        scanf("%d",&arr[i]); 
-    } 
-    printf("\nThe array  is "); 
-    for(int i=0;i<size;i++){ 
-        printf("%d",arr[i]); 
+#include <stdio.h>
+#define MAX_SIZE 50
+
+void main(){
+    int arr[MAX_SIZE];
+    int i, N, sum=0, max, min;
+    float average;
+    
+
+    printf("Enter size of array: ");
+    scanf("%d", &N);
+
+    printf("Enter %d elements in the array : ", N);
+    for(i=0; i<N; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
     }
-} 
+    printf("\nElements in array are: ");
+    for(i=0; i<N; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+    average = sum/N;
+    printf("\nAverage = %.2f", average);
+    
+    max = arr[0];
+    min = arr[0];
+    for(i=1; i<N; i++){
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("\nMaximum element = %d\n", max);
+    printf("Minimum element = %d", min);
+}
